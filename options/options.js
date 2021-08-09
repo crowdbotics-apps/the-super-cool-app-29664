e170438d32852e7043809bd7e9f7516cc61e125c
Crowdbotics <team@crowdbotics.com>
2021-08-09 13:30:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "the_super_cool_app_29664Identifier",
+  url: "https://the_super_cool_app_29664Identifier.botics.co",
+  api: "https://the_super_cool_app_29664Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
